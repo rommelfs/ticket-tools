ee234fb4576ea6391f163c58fcbe12165415253c
Sascha Rommelfangen <sascha@rommelfangen.de>
2018-06-28 14:18:22 +0200

    clean-up

--- get-reports.sh
+++ get-reports.sh
@@ -9,7 +9,7 @@ then
     cat get-reports.inc |grep ")"| cut -d ")" -f1 |grep -v "*"
     exit 1
 fi
-. ./get-reports.inc $1
+. ./get-reports.inc "$1"
 if [[ $LAST = "No matching results." ]]
 then
   echo "no matching tickets"
@@ -18,34 +18,34 @@ fi
 for tn in $LAST 
 do
     echo "Processing ticket #$tn"
-    URL=`./rt show $tn |egrep -o "h[tx][tx]ps?://[^ ]+" | head -n 1` 
+    URL=`$RT_BIN show $tn |egrep -o "h[tx][tx]ps?://[^ ]+" | head -n 1` 
     #./rt show $tn 
     URL=`echo $URL|sed -e "s/h[xX][xX]p\:/http\:/"`
     URL=`echo $URL|sed -e "s/h[xX][xX]ps\:/https\:/"`
-    echo $URL
-    UA_RESULT="`echo $URL | python /home/urlabuse/server.py`"
+    UA_RESULT="`echo $URL | $URLABUSE_BIN`"
     echo "$UA_RESULT"
     echo "Take-down consideration"
     echo "Emails" 
     echo "$UA_RESULT" | grep "All emails" | cut -d ":" -f2
+    echo $URL
     read -rsn1 -p"press (1) for phishing, (2) for malware, (3) for defacement, (8) ignore, (9) ignore and close, (0) for exit" option;echo
     case $option in
     1)  echo "Phishing server take-down request"
-        python /home/rommelfs/ticket-tools/create_ticket_with_template.py $tn /home/rommelfs/ticket-tools/templates/phishing_server.tmpl $URL False
+        $CREATETICKET_BIN $tn $TEMPLATE_PHISHING $URL False
         #/opt/rt4/bin/rt resolve $tn
-        /opt/rt4/bin/rt edit $tn set queue="Incidents" CF-Classification="Phishing"
+        $RT_BIN edit $tn set queue="Incidents" CF-Classification="Phishing"
         ;;
     2)  echo "Malware server take-down request"
-        python /home/rommelfs/ticket-tools/create_ticket_with_template.py $tn /home/rommelfs/ticket-tools/templates/malicious_files_hosted.tmpl $URL False
+        $CREATETICKET_BIN $tn $TEMPLATE_MALWARE $URL False
         #/opt/rt4/bin/rt resolve $tn
-        /opt/rt4/bin/rt edit $tn set queue="Incidents" CF-Classification="Malware"
+        $RT_BIN edit $tn set queue="Incidents" CF-Classification="Malware"
         ;;
     3)  echo "Defaced server take-down request"
-        python /home/rommelfs/ticket-tools/create_ticket_with_template.py $tn /home/rommelfs/ticket-tools/templates/defaced_website.tmpl $URL False
+        $CREATETICKET_BIN $tn $TEMPLATE_DEFACEMENT $URL False
         #/opt/rt4/bin/rt resolve $tn
-        /opt/rt4/bin/rt edit $tn set queue="Incidents" CF-Classification="System Compromise"
+        $RT_BIN edit $tn set queue="Incidents" Classification="System Compromise"
         ;;
-    9)  /opt/rt4/bin/rt resolve $tn
+    9)  $RT_BIN resolve $tn
         ;;
     0)  exit
         ;;
